94c067403a78d622e7804a85112c57d690452d41
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-05 17:58:03 +0000

    Variable

---
+++ javaScript/variable.js
@@ -0,0 +1,7 @@
+const accountId= 14656;
+let accountEmail = "raj381289@gmail.com";
+let accountPassword = "801105";
+accountCity = "Jaipur";
+accountEmail ="kumarrrankit2002@gmail.com";
+accountCity = "Patna";
+console.table([accountCity, accountEmail, accountPassword, accountId]);
