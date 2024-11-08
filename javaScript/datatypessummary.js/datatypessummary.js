05fd21599ca447b780185c95c8ac5fbe085839c1
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-08 10:57:24 +0000

    all about datatypes

---
+++ javaScript/datatypessummary.js/datatypessummary.js
@@ -0,0 +1,32 @@
+// 2 part  primitive and non-primitive datatypes
+
+// Primitive types :- 7 types: string, Number, Boolean, null,undefined, Symbol(uniqueness),BigInt
+
+//Reference types/ Non-primitive :- Arrays, Objects, Functions
+
+//javascript is 
+//JavaScript is dynamically typed. This means variable types are determined at runtime, 
+//and you don’t need to declare the type of a variable when you create it.
+
+const score = 100
+const scoreValue= 100.9
+const isLoggedIn = false
+const outsideTemp = null
+const id = Symbol('1233');
+const anotherId = Symbol('1233');
+console.log(id === anotherId);
+console.log(id)
+console.log(anotherId)
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj ={
+    name:"amit Kumar", age:22, uid:"22bcs14656"
+
+}
+
+// functions
+
+const myFunction = function(){
+    console.log("meHuDon")
+}
+console.log(myFunction)
+    console.log (anotherId)
