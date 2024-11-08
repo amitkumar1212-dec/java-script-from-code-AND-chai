dbfeb10b87cd245e3b66b932d24b78c3f20015fb
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-08 10:27:50 +0000

    comapriosn hai

---
+++ comparison_in_datatypes.js
@@ -0,0 +1,8 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//value hai clean code ye fuddu code hai upar toh..!
