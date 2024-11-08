e15ac43722aba30d9955e460fe3a947c8eb84ebd
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-08 10:06:44 +0000

    Strings JS

---
+++ javaScript/string.js
@@ -0,0 +1,14 @@
+console.log("Hello jii ");
+
+                                                                                    // operations
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " Amit"
+let str3 = str1+str2;
+console.log(str3)
+
+console.log(+true);
