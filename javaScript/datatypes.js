1880822df39430d2e927f417a2d66a0cea955fed
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-08 10:04:12 +0000

    basics of datatypes in js

---
+++ javaScript/datatypes.js
@@ -0,0 +1,28 @@
+
+
+console.log("heelllu")
+"use strict"
+let age = "anmit";
+//number = 2 to power 53
+//big int
+//string= ""
+//boolean = true/false
+//null = sstandalone value
+//undefined
+//symbol = uniqueness ke liye
+
+
+//object
+
+console.log (typeof age) 
+//Functions
+//conversions operations
+let score=33
+console.log(typeof score)
+let valueInNum = Number (score)
+console.log(typeof valueInNum)
+
+//"33" is easily convert
+//Nan not in number 
+//true=1; false =0;
+
