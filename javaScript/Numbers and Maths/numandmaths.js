006aa256dc2a9417c5bf113ff60ae7d9f304275f
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-10 18:30:59 +0000

    Here talking about numbers and Maths

---
+++ javaScript/Numbers and Maths/numandmaths.js
@@ -0,0 +1,24 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);// Number is now fixed as 100 as Number is datatypes
+
+console.log(balance);
+
+console.log(balance.toString().length);// strings property to show length of var
+
+console.log(balance.toFixed(2)); //strings property to fixed the balance like 100.00
+
+console.log(balance.toPrecision(3))//strings property to show precision and automatically defined the round numbers
+
+const otherNum = 636.09876
+
+console.log(otherNum.toPrecision(3))
+
+let hundreds = 10000;
+console.log(hundreds.toLocaleString());
+
+
+
+// =======================================MATHS=========================================================================
+
