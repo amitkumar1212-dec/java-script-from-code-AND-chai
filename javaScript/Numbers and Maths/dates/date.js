f433a622a9275883e682fa065eee0bae7c914143
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-11 07:39:47 +0000

    Here in this date and time we know about the time and date and also i use the extension namely code runner!

---
+++ javaScript/Numbers and Maths/dates/date.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toISOString())
+
+// let myDate = new Date();
+// console.log(myDate.getDate());
+// myDate.toLocaleString('dafault', {
+//     weekday: "long"
+// })
