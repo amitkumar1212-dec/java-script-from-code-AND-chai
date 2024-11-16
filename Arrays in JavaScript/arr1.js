be202b76f38378940744b6cadbe087bc36da24aa
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-16 06:33:01 +0000

    all about arrays

---
+++ Arrays in JavaScript/arr1.js
@@ -0,0 +1,13 @@
+const myArr = [1,4,6,77,334,0];
+console.log( " THe array is like :",myArr);
+
+// Array methods
+// myArr.push(45)
+// myArr.pop()
+// myArr.unshift(12)
+myArr.shift()
+console.log( "The new after applied method on array is like:", myArr)
+
+
+// myArr.unshift(10);
+// myArr.shift()
