daceab98143f84a1d7888aa7d68b089dda7a48a8
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-11-26 09:51:17 +0000

    objects

--- Objects/obj2.js
+++ Objects/obj2.js
@@ -1,2 +1,32 @@
-console.log("Object2 ")
-const tinderUser = new O
+// console.log("hello ")
+// console.log("Here, in this file we learn a bout some objects methods")
+// const tinderUser = new Object() // Singleton Object
+// console.log(tinderUser)
+const tinderUser ={}
+ tinderUser.id = "123xyz";
+ tinderUser.name ="JohnSnow";
+ tinderUser.isLoggedIn = false;
+// console.log(tinderUser);
+const regularUsser = {
+    email:"raj381289@gmail.com",
+    fullname:{
+        usersFullname:{
+            firstName: "amit",
+            lastname:"Jii"
+        }
+    }
+}
+// console.log(regularUsser.fullname?.usersFullname)
+
+const obj1 = {1: "a ", 2:"b"}
+const obj2 ={3:"j", 4:"k"}
+
+// const obj3= Object.assign({}, obj1, obj2) // empty object 
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2} //spread
+// console.log(obj3)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
