04af9eece719ce772798b64edd254c17d44d6d09
amitkumar1212-dec <kumarrrankit2002@gmail.com>
2024-12-19 04:55:42 +0000

    changed

--- Objects/obj2.js
+++ Objects/obj2.js
@@ -16,17 +16,17 @@ const regularUsser = {
         }
     }
 }
-// console.log(regularUsser.fullname?.usersFullname)
+// console.log(regularUsser.fullname?.usersFullname) //values 
 
 const obj1 = {1: "a ", 2:"b"}
 const obj2 ={3:"j", 4:"k"}
 
-// const obj3= Object.assign({}, obj1, obj2) // empty object 
+// const obj3= Object.assign({}, obj1, obj2) // empty object /All values are assingned 
 // console.log(obj3)
 
-const obj3 = {...obj1, ...obj2} //spread
+const obj3 = {...obj1, ...obj2} //spread is used instead of assinged value which is {};
 // console.log(obj3)
 console.log(Object.keys(tinderUser))
 console.log(Object.values(tinderUser))
 console.log(Object.entries(tinderUser))
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) // methods to ask does have or not?
